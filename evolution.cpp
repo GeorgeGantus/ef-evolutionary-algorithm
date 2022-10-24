c86c064d85e4c66a85d55d6a2c969d66767ace78
George Gantus <george.gantus@dotgroup.com.br>
2022-10-24 11:16:37 -0300

    change params to int

--- evolution.cpp
+++ evolution.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-double get_rand() {
-    return (double) rand()/RAND_MAX;
+float get_rand() {
+    return (float) rand()/RAND_MAX;
 }
 
 int get_max_index(vector<float> &array) {
@@ -26,12 +26,9 @@ int get_max_index(vector<float> &array) {
     return i;
 }
 
-pair<vector<float>,int> get_best(vector<vector<float>> &population, vector<float> &results) {
-    pair<vector<float>, int> ret;
+int get_best(vector<vector<int>> &population, vector<float> &results) {
     int max_index = get_max_index(results);
-    ret.first = population[max_index];
-    ret.second = max_index;
-    return ret;
+    return max_index;
 }
 
 int rand_sign() {
@@ -42,22 +39,23 @@ int rand_sign() {
     }
 }
 
-float get_single_mutation() {
-    return (get_rand()*rand_sign()*MUTATION_RATE);
+int get_single_mutation() {
+    return static_cast<int>(get_rand()*rand_sign()*MUTATION_RATE);
 }
 
-vector<float> mutate(vector<float> &params) {
-    vector<float> mutated;
+// Needs refactor
+vector<int> mutate(vector<int> &params) {
+    vector<int> mutated;
     for (int i = 0; i < params.size(); i++) {
         mutated.push_back(params[i] + get_single_mutation());
     }
     return mutated;
 }
 
-vector<vector<float>> mutate_population(vector<vector<float>> &population, int best_index) {
-    vector<vector<float>> mutated_population;
+vector<vector<int>> mutate_population(vector<vector<int>> &population, int best_index) {
+    vector<vector<int>> mutated_population;
     for (int i = 0; i < population.size(); i++){
-        vector<float> params = population[i];
+        vector<int> params = population[i];
         if (i != best_index) {
             params = mutate(population[i]);
         }
@@ -66,11 +64,11 @@ vector<vector<float>> mutate_population(vector<vector<float>> &population, int b
     return mutated_population;
 }
 
-vector<vector<float>> elitism(vector<float> &best, vector<vector<float>> &population) {
-    vector<vector<float>> bred_population;
+vector<vector<int>> elitism(vector<int> &best, vector<vector<int>> &population) {
+    vector<vector<int>> bred_population;
 
-    for (vector<float> ind : population) {
-        vector<float> bred_ind;
+    for (vector<int> ind : population) {
+        vector<int> bred_ind;
         for (int i = 0; i < best.size(); i++){
             bred_ind.push_back((best[i] + ind[i]) / 2);
         }
@@ -80,20 +78,13 @@ vector<vector<float>> elitism(vector<float> &best, vector<vector<float>> &popula
 
 }
 
-void print_vector (vector<float> test) {
-    for (float a : test){
+void print_vector (vector<int> array) {
+    for (int a : array){
         cout << a << endl;
     }
 }
-void print_population(vector<vector<float>> population) {
-    cout << "### PRINT POPULATION ###" << endl;
-    for (vector<float> ind : population){
-        cout << "ind" << endl;
-        print_vector(ind);
-    }
-}
 
-string params_to_command(string cmd, vector<float> &params) {
+string params_to_command(string cmd, vector<int> &params) {
     for (int i = 0; i < params.size(); i++){
         cmd += " " + to_string(params[i]);
     }
@@ -117,8 +108,8 @@ float get_fitness(char *command) {
     return fitness;
 }
 
-vector<vector<float>> intiialize_population(vector<float> seed, int pop_size){
-    vector<vector<float>> population;
+vector<vector<int>> intiialize_population(vector<int> seed, int pop_size){
+    vector<vector<int>> population;
     population.push_back(seed);
     for (int i = 0; i < pop_size -1; i++){
         population.push_back(mutate(seed));
@@ -131,28 +122,28 @@ int main () {
 
     system("g++ test.cpp");
 
-    vector<float> pop_seed = {10,10};
-    vector<vector<float>> population;
+    vector<int> pop_seed = {10,10};
+    vector<vector<int>> population;
     population = intiialize_population(pop_seed, POPULATION_SIZE);
 
 
-    vector<float> best;
+    vector<int> best;
     for (int i = 0; i < 200; i++){
 
         vector<float> results;
+
         for (int j = 0; j < population.size(); j++){
             string command_s = params_to_command("./a.out", population[j]);
             char command[command_s.length() + 1];
             strcpy(command, command_s.c_str());
+
             float fitness = get_fitness(command);
             results.push_back(fitness);
         }
-        pair<vector<float>, int> best_data = get_best(population, results);
-        best = best_data.first;
-        int best_index = best_data.second;
-        /* print_vector(results);
-        cout << "BEST_INDEX: " <<best_index << endl;
-        cout << "#######" << endl; */
+
+        int best_index = get_best(population, results);
+        best = population[best_index];
+
         population = elitism(best,population);
         population = mutate_population(population, best_index);
     }
